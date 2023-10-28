3a5a2a32ff254a0ad29c53827f0308daeaf76de5
Tulsi <tulsisatasiya085@gmail.com>
2023-10-28 12:11:29 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Dashboard from './Components/Dashboard';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Menu from './Components/Menu';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Menu/>
+      <Dashboard/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
